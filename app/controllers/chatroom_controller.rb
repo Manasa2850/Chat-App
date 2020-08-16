2d166ce3801b7eff36aa99ebd8c71a271485e973
Manasa2850 <43354059+Manasa2850@users.noreply.github.com>
2020-08-16 22:49:43 +0530

    Add files via upload

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,8 +1,9 @@
 class ChatroomController < ApplicationController
-    before_action :require_user
-    
-    def home
-        @message = Message.new
-        @messages = Message.all
-    end
+  before_action :require_user
+
+  def index
+    @message = Message.new
+    @messages = Message.custom_display
+  end
+
 end
